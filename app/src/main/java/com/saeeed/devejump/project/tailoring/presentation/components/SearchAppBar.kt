9968951ea9544ac5f9a515c251e187e8bee7b7fb
saeed <ghfarisaeed1@gmail.com>
2023-10-11 13:29:19 +0330

    currect search app bar

--- app/src/main/java/com/saeeed/devejump/project/tailoring/presentation/components/SearchAppBar.kt
+++ app/src/main/java/com/saeeed/devejump/project/tailoring/presentation/components/SearchAppBar.kt
@@ -6,22 +6,31 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.saeeed.devejump.project.tailoring.presentation.ui.list.Category
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchAppBar(
     query: String,
@@ -30,14 +39,14 @@ fun SearchAppBar(
     categories: List<Category>,
     selectedCategory: Category?,
     onSelectedCategoryChanged: (String) -> Unit,
-    scrollPosition: Float,
-    onChangeScrollPosition: (Float) -> Unit,
+    scrollPosition: Int,
+    onChangeScrollPosition: (Int) -> Unit,
 ) = Surface(
     modifier = Modifier
         .fillMaxWidth()
     ,
     color = Color.White,
-    elevation = 8.dp,
+    shadowElevation = 8.dp,
 ){
     Column{
         Row(modifier = Modifier.fillMaxWidth()){
@@ -47,55 +56,62 @@ fun SearchAppBar(
                     .padding(8.dp)
                 ,
                 value = query,
+
                 onValueChange = {
                     onQueryChanged(it)
                 },
                 label = {
                     Text(text = "Search")
                 },
-                keyboardOptions = KeyboardOptions(
+               keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        onExecuteSearch()
+                    }
                 ),
                 leadingIcon = {
                     Icon(Icons.Filled.Search)
                 },
-                onImeActionPerformed = { action, softKeyboardController ->
-                    if (action == ImeAction.Done) {
-                        onExecuteSearch()
-                        softKeyboardController?.hideSoftwareKeyboard()
-                    }
-                },
-                textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                backgroundColor = MaterialTheme.colors.surface
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor=MaterialTheme.colorScheme.onSurface,
+                    placeholderColor = MaterialTheme.colorScheme.surface,
+                    disabledPlaceholderColor= MaterialTheme.colorScheme.surface)
+
             )
+
         }
-        val scrollState = rememberScrollState()
+        val scrollState = rememberLazyListState()
+        val coroutineScope= rememberCoroutineScope()
         LazyRow(
             modifier = Modifier
-                .padding(start = 8.dp, bottom = 8.dp)
-            ,
-            scrollState = scrollState,
+                .padding(start = 8.dp, bottom = 8.dp),
+            state = scrollState
         ) {
+            coroutineScope.launch {
+                scrollState.scrollToItem(scrollPosition,0)
 
-            // restore scroll position after rotation
-            scrollState.scrollTo(scrollPosition)
-
-            for(category in categories){
-                CategoryChip(
-                    category = category.value,
-                    isSelected = selectedCategory == category,
-                    onSelectedCategoryChanged = {
-                        onChangeScrollPosition(scrollState.value)
-                        onSelectedCategoryChanged(it)
-                    },
-                    onExecuteSearch = {
-                        onExecuteSearch()
-                    },
-                )
             }
+
         }
-    }
+        for(category in categories){
+            CategoryChip(
+                category = category.value,
+                isSelected = selectedCategory == category,
+                onSelectedCategoryChanged = {
+                    onChangeScrollPosition(scrollState.hashCode())
+                    onSelectedCategoryChanged(it)
+                },
+                onExecuteSearch = {
+                    onExecuteSearch()
+                },
+            )
+        }
+
+        }
+
 }
 
 fun Icon(search: Any) {
