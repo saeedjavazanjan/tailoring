fa9958b8611969780a6b68eb587b63faa72841c0
saeed <ghfarisaeed1@gmail.com>
2023-11-04 14:22:52 +0330

    fix some problems of description

--- app/src/main/java/com/saeeed/devejump/project/tailoring/presentation/ui/description/DescriptionScreen.kt
+++ app/src/main/java/com/saeeed/devejump/project/tailoring/presentation/ui/description/DescriptionScreen.kt
@@ -1,6 +1,7 @@
 package com.saeeed.devejump.project.tailoring.presentation.ui.description
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ScaffoldState
@@ -16,10 +17,11 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.saeeed.devejump.project.tailoring.presentation.components.SewMethodView
 import com.saeeed.devejump.project.tailoring.ui.theme.AppTheme
+import com.saeeed.devejump.project.tailoring.utils.TAG
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "SuspiciousIndentation")
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalCoroutinesApi::class)
 @Composable
 fun DescriptionScreen(
@@ -34,10 +36,10 @@ fun DescriptionScreen(
     }else {
         // fire a one-off event to get the recipe from api
         val onLoad = viewModel.onLoad.value
-        if (!onLoad) {
-            viewModel.onLoad.value = true
+       if (!onLoad) {
+           viewModel.onLoad.value = true
             viewModel.onTriggerEvent(SewEvent.GetSewEvent(sewId))
-        }
+       }
 
         val loading = viewModel.loading.value
 
